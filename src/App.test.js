710d3a7daee3087c3fa796bf51b0567528c1e5b2
lc <gingeraleisthebest@gmail.com>
2022-11-26 21:53:41 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { describe } from 'node:test';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe();
